243110916526a1df309ebf7ad3bc847dd84fda18
@parou.char <curlycode@outlook.com>
2024-10-02 19:10:28 +0300

    implement lesson

--- langedu/app/Http/Controllers/LessonController.php
+++ langedu/app/Http/Controllers/LessonController.php
@@ -1,10 +1,55 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Lesson;
 use Illuminate\Http\Request;
 
 class LessonController extends Controller
 {
-    //
+
+    //index method
+    public function index(){
+        $lessons = Lesson::latest()->get();
+        return view('lessons.indexLessons',['lessons' => $lessons]);
+    }
+
+    //show method
+    public  function show($id){
+        $lesson = Lesson::findOne($id);
+        return view('lessons.showLesson',['lesson' => $lesson]);
+    }
+
+    //create method
+
+    public function create(){
+        return view('lessons.createLesson');
+    }
+
+    //store method
+    public function store(Request $request){
+        $request->validate([
+            'title'=> 'required|max_length:255',
+            'script'=>'required|max_length:255'
+        ]);
+
+        Lesson::create([
+            'title'=>$request->input('title'),
+            'script'=>$request->input('script')
+        ]);
+
+        return redirect()->back();
+    }
+    //edit method
+    public function edit($id){
+        $editLesson = Lesson::findOrFail($id);
+        return view('lessons.editLesson',['editLesson'=>$editLesson]);
+    }
+    //destroy method
+
+    public function destroy($id){
+        $destroyLesson = Lesson::findOrFail($id);
+        $destroyLesson.delete();
+        return redirect()->back();
+    }
+
 }
--- langedu/routes/web.php
+++ langedu/routes/web.php
@@ -2,14 +2,42 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-
+use App\Http\Controllers\StudentsController;
+use App\Http\Controllers\LessonController;
 
 
 //Dashboard routes
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+//Students routes
+//index routes
+Route::get('students',[StudentsController::class,'index'])->name('students.indexStudents');
+Route::get('student/{student:id}',[StudentsController::class,'show'])->name('students.showStudent');
+//create routes
+Route::get('/create',[StudentsController::class,'create'])->name('students.createStudent');
+Route::post('create',[StudentsController::class,'store']);
+//update routes
+Route::get('/update/{student:id}',[StudentsController::class,'edit'])->name('students.editStudent');
+Route::put('update/{student:id}',[StudentsController::class,'update'])->name('students.update');
+//---------------
+//delete routes
+Route::delete('/delete/{student:id}',[StudentsController::class,'destroy'])->name('students.delete');
+
+// Lesson routes
+Route::get('lessons', [LessonController::class, 'index'])->name('lessons.indexLessons');
+Route::get('/{lesson:id}', [LessonController::class, 'show'])->name('lessons.showLesson');
+
+// Create routes
+Route::get('/create', [LessonController::class, 'create'])->name('lessons.createLesson');
+Route::post('/create', [LessonController::class, 'store']);
+
+// Update routes
+Route::get('/update/{lesson:id}', [LessonController::class, 'edit'])->name('lessons.editLesson');
+Route::put('/update/{lesson:id}', [LessonController::class, 'update'])->name('lessons.update');
+
+// Delete routes
+Route::delete('/delete/{lesson:id}', [LessonController::class, 'delete'])->name('lessons.delete');
 
 
 
