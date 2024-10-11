5c63b4582b1cd2ae565a99bc1f7926c98474d311
@parou.char <curlycode@outlook.com>
2024-10-11 06:27:19 +0300

    make model for multiple choice exercise

---
+++ langedu/app/Models/MultipleChoiceExercise.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MultipleChoiceExercise extends Model
+{
+    use HasFactory;
+}
