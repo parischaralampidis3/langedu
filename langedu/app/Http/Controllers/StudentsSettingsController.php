99391c6b82f0ab8ffbcc36ebe3637da4bd6bb847
@parou.char <curlycode@outlook.com>
2024-10-21 19:48:19 +0300

    working at stutent setting controller  methods

--- langedu/app/Http/Controllers/StudentsSettingsController.php
+++ langedu/app/Http/Controllers/StudentsSettingsController.php
@@ -8,23 +8,25 @@ use App\Models\StudentSettings;
 
 class StudentsSettingsController extends Controller
 {
-    public function togglLimitStudent(Request $request, $id){
-        $toggleStudent = Students::find($id);
-        if(!$toggleStudent){
-            return response()->json(['error'=>'Student not found'],404);
-        }
+    public function toggleLimitStudent(Request $request, $id){
+        $toggleStudent = Students::findOrFail($id);
+  
         $studentSetting = $toggleStudent->studentSettings;
-        if(!$studentSetting){
+      
             $request->validate([
                 'count_limit_students' => 'boolean',
                 'limit_students_number'=> 'required|integer|min:1'
             ]);
+
             StudentSettings::create([
                 'student_id' => $toggleStudent->id,
                 'count_limit_students' => $request->input('count_limit_students'),
                 'limit_students_number' => $request ->input('limit_students_number')
             ]);
-        };
+
+         if(!$studentSetting){ 
+               return response()->json(['message' => 'Settings have not updated successfully'],404);
+         }
         
         if($studentSetting){
             $studentSetting-> count_limit_students = $request->input('count_limit_students');
